19cf2710dcc754734103eff036adcdab5eee0eff
Kelsey OConnell <kelseyoconnell2217@gmail.com>
2024-09-06 10:22:13 -0400

    core deliverables

--- src/index.js
+++ src/index.js
@@ -1,21 +1,82 @@
-// index.js
+const menu = document.getElementById("ramen-menu")
 
 // Callbacks
 const handleClick = (ramen) => {
-  // Add code
+  let ramenId = ramen.target.id
+  fetch(`http://localhost:3000/ramens/${ramenId}`)
+    .then(resp=>resp.json())
+    .then(ramenInfo=>showDetails(ramenInfo))  
 };
 
-const addSubmitListener = () => {
-  // Add code
+function showDetails(ramen){
+  let img = document.querySelector('.detail-image')
+  img.src = ramen.image
+  let name = document.querySelector('.name')
+  name.textContent = ramen.name
+  let restaurant = document.querySelector('.restaurant')
+  restaurant.textContent = ramen.restaurant
+  let rating = document.getElementById('rating-display')
+  rating.textContent = ramen.rating
+  let comment = document.getElementById('comment-display')
+  comment.textContent = ramen.comment
+} 
+
+const addSubmitListener = (e) => {
+  e.preventDefault()
+  let nameInput= e.target["new-name"].value
+  //fetch last id
+  let idInput
+    fetch('http://localhost:3000/ramens')
+    .then(resp=>resp.json())
+    .then(ramensArray=>{
+      let lastID = ramensArray[ramensArray.length-1].id
+      idInput = lastID+1
+    })
+  
+  let restaurantInput = e.target["new-restaurant"].value
+  let imageInput = e.target["new-image"].value
+  let ratingInput = e.target["new-rating"].value
+  let commentInput = e.target["new-comment"].value
+  fetch('http://localhost:3000/ramens', {
+    'method': 'POST',
+    'headers': {'Content-type': 'application/json'},
+    'body' : JSON.stringify({
+        "id": idInput,
+        "name": nameInput,
+        "restaurant": restaurantInput,
+        "image": imageInput,
+        "rating": ratingInput,
+        "comment": commentInput
+    })
+    
+
+  })
+  displayRamens
 }
 
 const displayRamens = () => {
-  // Add code
+  fetch('http://localhost:3000/ramens')
+    .then(resp=>resp.json())
+    .then(jsonResponse=>{
+      showDetails(jsonResponse[0]);
+      jsonResponse.forEach(
+      (jsonResponse)=>{
+        let img = document.createElement('img')
+        img.src=jsonResponse.image
+        img.id=jsonResponse.id
+        img.addEventListener('click', handleClick)
+        menu.appendChild(img)
+
+      }
+    )})
 };
 
+let form = document.getElementById("new-ramen")
+
+
 const main = () => {
-  // Invoke displayRamens here
-  // Invoke addSubmitListener here
+  document.addEventListener('DOMContentLoaded', displayRamens)
+    form.addEventListener('submit', addSubmitListener)
 }
 
 main()
